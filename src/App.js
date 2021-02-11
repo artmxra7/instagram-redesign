9d85dac4d540a1cc50e03c1cf124c3f86304e3b3
Erwin RA <artmxarea@gmail.com>
2021-02-12 05:58:42 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+import './app.styles.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <div className='text-black font-bold rounded-lg border shadow-lg p-10 m-20'>
+          <div></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
